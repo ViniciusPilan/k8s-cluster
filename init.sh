23a15a5a8804828d8e70cf7fe95e106c4e6cbefe
ViniciusPilan <vinipilan@gmail.com>
2024-06-20 23:39:50 -0300

    fix(init pipeline): fix wrong exec of secret discovery

--- init.sh
+++ init.sh
@@ -7,12 +7,12 @@ sleep 10
 echo "<----------- Installing Argo ----------->" 
 kubectl apply -f argo/install -n argocd
 sleep 20
-kubectl get secrets -n argocd argocd-initial-admin-secret -o yaml | grep password | cut -d " " -f 4 | base64 --decode > /home/vinipilan/documents/k8s-cluster/argocd-password.txt
-
 
 echo "<----------- Installing Argo Applications ----------->"
 kubectl apply -f argo/applications -n argocd
 sleep 5
+kubectl get secrets -n argocd argocd-initial-admin-secret -o yaml | grep password | cut -d " " -f 4 | base64 --decode > /home/vinipilan/documents/k8s-cluster/argocd-password.txt
+
 
 echo "<-------------------- OUTPUTS -------------------->"
 echo "The cluster was created!"
