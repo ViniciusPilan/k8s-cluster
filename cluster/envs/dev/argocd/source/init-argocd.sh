bec0d36ebed83c381c4ca878b4f2a6364b2976c7
ViniciusPilan <vinipilan@gmail.com>
2024-08-04 10:03:49 -0300

    feat(grafana): install grafana

--- cluster/envs/dev/argocd/source/init-argocd.sh
+++ cluster/envs/dev/argocd/source/init-argocd.sh
@@ -1,7 +1,6 @@
 kubectl apply -f argocd-ns.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml
 
-sleep 10
-
+sleep 20
 kubectl get secrets -n argocd argocd-initial-admin-secret -o yaml | grep password | awk '{print $2}' | base64 --decode
 kubectl port-forward -n argocd service/argocd-server 8080:80
