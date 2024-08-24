7b5047eb18498764f593567d6d54d908afa04122
ViniciusPilan <vinipilan@gmail.com>
2024-08-24 12:43:45 -0300

    feat: new structure

--- cluster/envs/dev/apps/argocd/source/init-argocd.sh
+++ cluster/envs/dev/apps/argocd/source/init-argocd.sh
@@ -2,5 +2,10 @@ kubectl apply -f argocd-ns.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml
 
 sleep 20
+
+kubectl apply -f ../applications
+
+sleep 5
+
 kubectl get secrets -n argocd argocd-initial-admin-secret -o yaml | grep password | awk '{print $2}' | base64 --decode
 kubectl port-forward -n argocd service/argocd-server 8080:80
