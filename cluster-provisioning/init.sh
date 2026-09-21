8ad4dd11b0efd78c74c08dcaa32e5516011c446c
Vinicius Pilan <vinipilan@gmail.com>
2026-09-21 10:12:39 -0300

    feat: create argo applications app in the cluster init

--- cluster-provisioning/init.sh
+++ cluster-provisioning/init.sh
@@ -12,8 +12,15 @@ function createBase() {
 
 function installArgo() {
     echo "INFO: Installing ArgoCD"
+
+    echo "step 01: add repository to help repo list"
     helm repo add argo https://argoproj.github.io/argo-helm
-    helm install argocd argo/argo-cd --values=../tools/argo/values.yaml --namespace=argocd --create-namespace=true
+
+    echo "step 02: install Helm release and wait all be ready to proceed"
+    helm install argocd argo/argo-cd --values=../tools/argo/values.yaml --namespace=argocd --create-namespace=true --wait
+
+    echo "step 03: create argo applications to manage all repository applications"
+    kubectl apply -f ../tools/argo/argo-applications.yaml
 }
 
 
